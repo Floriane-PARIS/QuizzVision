bf969730da5d745fbabdede5c93c13346617a3f9
Floriane Paris <floriane99.paris@free.fr>
2021-03-26 11:44:58 +0100

    Configuration-form #1

---
+++ front-end/src/app/configurations/configuration-form/configuration-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-form',
+  templateUrl: './configuration-form.component.html',
+  styleUrls: ['./configuration-form.component.scss']
+})
+
+export class ConfigurationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
