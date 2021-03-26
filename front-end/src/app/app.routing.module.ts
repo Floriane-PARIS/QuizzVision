25bff0757c197f4f9e6e3a5580951fe3ef78a818
Floriane Paris <floriane99.paris@free.fr>
2021-03-26 16:54:35 +0100

    page configuration vide réussis #1

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { ConfigurationListComponent } from './configurations/configuration-list/configuration-list.component';
 import { GameStartComponent } from './games/game-start/game-start.component';
 import { GameQuestionComponent } from './questions/game-question/game-question.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
+    {path: 'configuration-list', component: ConfigurationListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
     {path: 'game-start/:id', component: GameStartComponent},
     {path: 'game-question/:id', component: GameQuestionComponent},
