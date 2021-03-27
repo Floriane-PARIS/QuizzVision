13b2320e11206a0123f4882b409f72f57601ab7b
DIJOUDFanny <fanny.dijoud@etu.unice.fr>
2021-03-27 10:20:36 +0100

    initializeGameForm in game-start

--- front-end/src/app/games/game-start/game-start.component.ts
+++ front-end/src/app/games/game-start/game-start.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Quiz } from 'src/models/quiz.model';
 import { QuizService } from 'src/services/quiz.service';
-
 import { Game } from '../../../models/game.model';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-game-start',
@@ -13,14 +13,22 @@ import { Game } from '../../../models/game.model';
 export class GameStartComponent implements OnInit {
 
   public quiz: Quiz;
+  public gameForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+  constructor(public formBuilder: FormBuilder, private route: ActivatedRoute, private quizService: QuizService) {
     this.quizService.quizSelected$.subscribe((quiz) => this.quiz = quiz);
   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.quizService.setSelectedQuiz(id);
+  }
+
+  private initializeGameForm(): void {
+    this.gameForm = this.formBuilder.group({
+      quizId: this.quiz.id,
+      questionId: this.quiz.questions.pop().id
+    });
   }
 
 }
