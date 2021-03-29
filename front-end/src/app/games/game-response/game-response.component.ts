29f6b7cf22f2df7ff60db7c50271e2d0864440e6
DIJOUDFanny <fanny.dijoud@etu.unice.fr>
2021-03-29 17:11:37 +0200

    visualisation réponse choisie ok

---
+++ front-end/src/app/games/game-response/game-response.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Answer, Question} from '../../../models/question.model';
+
+@Component({
+  selector: 'app-game-response',
+  templateUrl: './game-response.component.html',
+  styleUrls: ['./game-response.component.scss']
+})
+export class GameResponseComponent implements OnInit {
+
+  @Input()
+  answer: Answer;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
