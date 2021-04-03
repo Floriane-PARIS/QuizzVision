098efb07c135a7d3bb991b37c14c3439c6723529
GhadaAJIMI <Ghada.ajimi@etu.univ-cotedazur.fr>
2021-04-03 19:50:57 +0200

    #5 ajouter le component edit-theme

---
+++ front-end/src/app/themes/edit-theme/edit-theme.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Theme} from '../../../models/theme.model';
+import {ActivatedRoute} from '@angular/router';
+import {ThemeService} from '../../../services/theme.service';
+
+@Component({
+  selector: 'app-edit-theme',
+  templateUrl: './edit-theme.component.html',
+  styleUrls: ['./edit-theme.component.scss']
+})
+export class EditThemeComponent implements OnInit {
+  public theme: Theme;
+
+  constructor(private route: ActivatedRoute, private themeService: ThemeService) {
+    this.themeService.themeSelected$.subscribe((theme) => this.theme = theme);
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.themeService.setSelectedTheme(id);
+  }
+}
