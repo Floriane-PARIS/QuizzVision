d000b58c530969e904b784624078504878960114
Floriane Paris <floriane99.paris@free.fr>
2021-04-04 18:43:25 +0200

    configuration bold is boolean #1

--- back-end/app/models/configuration.model.js
+++ back-end/app/models/configuration.model.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Configuration', {
-  bold: Joi.string().required(),
+  bold: Joi.boolean().required(),
   size: Joi.number().required(),
   police: Joi.string().required(),
 })
--- front-end/src/models/configuration.model.ts
+++ front-end/src/models/configuration.model.ts
@@ -1,6 +1,6 @@
 export interface Configuration {
     id: string;
-    bold: string;
+    bold: boolean;
     size: number;
     police: string;
 }
