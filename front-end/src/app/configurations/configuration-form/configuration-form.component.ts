d85c93a180643e4906c280f2079ad9b23338472b
NadineAzebaze <nadine-carel.azebaze-zewo@etu.univ-cotedazur.fr>
2021-04-16 11:08:59 +0200

    essaie #18 nok

--- front-end/src/app/configurations/configuration-form/configuration-form.component.ts
+++ front-end/src/app/configurations/configuration-form/configuration-form.component.ts
@@ -28,7 +28,7 @@ export class ConfigurationFormComponent implements OnInit {
       contrast: ['100'],
       shift: ['60']
     });
-    this.handicap = "Glaucome";
+    this.handicap = this.configurationForm['handicap'];
   }
 
   ngOnInit(): void {
