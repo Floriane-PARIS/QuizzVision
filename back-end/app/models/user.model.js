ee9e6aa9d199a312578c31fce1973239cf016490
NadineAzebaze <nadine-carel.azebaze-zewo@etu.univ-cotedazur.fr>
2021-04-18 12:35:03 +0200

    go to french

--- back-end/app/models/user.model.js
+++ back-end/app/models/user.model.js
@@ -4,8 +4,8 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('User', {
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
-  encadreur: Joi.string().required(),
+  encadreur: Joi.string(),
   maladies: Joi.string().required(),
-  commentaires: Joi.string().required(),
+  commentaires: Joi.string(),
   age: Joi.number().required(),
 })
