aaa706463c38f89a9f15dc60dee6c30d29d8396e
ghada <ghada.ajimi@etu.univ-cotedazur.fr>
2021-05-04 10:05:33 +0200

    #24 programmer le comportement du bouton modifier des quizs

--- front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
+++ front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { Quiz } from 'src/models/quiz.model';
 import { QuizService } from 'src/services/quiz.service';
+import {Theme} from '../../../models/Theme.model';
+import {ThemeService} from '../../../services/theme.service';
 
 @Component({
   selector: 'app-edit-quiz',
@@ -9,14 +11,18 @@ import { QuizService } from 'src/services/quiz.service';
   styleUrls: ['./edit-quiz.component.scss']
 })
 export class EditQuizComponent implements OnInit {
-
+  public quizTheme: string;
   public quizName: string;
+  public themes: Theme[];
   public quiz: Quiz;
 
-  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+  constructor(private route: ActivatedRoute, private quizService: QuizService, public themeService: ThemeService) {
     this.quizService.quizSelected$.subscribe((quiz) =>  {
       this.quiz = quiz;
       this.quizService.retrieveQuizzes();
+    });
+    this.themeService.themes$.subscribe((themes: Theme[]) => {
+      this.themes = themes;
     });
   }
 
@@ -28,5 +34,10 @@ export class EditQuizComponent implements OnInit {
   modifQuizName(): void {
     const newName: string = this.quizName as string;
     this.quizService.renameQuiz(this.quiz, newName);
+  }
+
+  modifQuizTheme(): void {
+    const newName: string = this.quizTheme as string;
+    this.quizService.changeQuizTheme(this.quiz, newName);
   }
 }
