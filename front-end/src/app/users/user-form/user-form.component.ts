c6ee095b25a0a8e5f7203bd7a1be26c770ca35be
ghada <ghada.ajimi@etu.univ-cotedazur.fr>
2021-05-11 10:48:09 +0200

    supprimer des commentaires unitiles

--- front-end/src/app/users/user-form/user-form.component.ts
+++ front-end/src/app/users/user-form/user-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 import { UserService } from '../../../services/user.service';
 import { User } from '../../../models/user.model';
 
