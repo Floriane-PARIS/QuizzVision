162ebd33318293846fa931e841a72f5dcbec8b32
Floriane Paris <floriane99.paris@free.fr>
2021-05-13 10:00:24 +0200

    Correction back-end animateur model #11

--- back-end/app/models/animateur.model.js
+++ back-end/app/models/animateur.model.js
@@ -3,9 +3,9 @@ const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Animateur', {
   name: Joi.string().required(),
-  mail: Joi.string().required,
+  mail: Joi.string().required(),
   password: Joi.string().required(),
-  passwordConfirmed: Joi.string().required,
+  passwordConfirmed: Joi.string().required(),
   //user: Joi.array(),
 
 })
